8580e154f47c1bf4bcdb3e1bd764be1030cad41b
Thiago Martins <thiago_valentim1@hotmail.com>
2025-08-01 09:43:25 -0300

    refactor(ticket): move ticket module to app folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './app/user/user.module';
 import { ConfigurationModule } from './libs/configuration/configuration.module';
-import { TicketModule } from './ticket/ticket.module';
+import { TicketModule } from './app/ticket/ticket.module';
 import { AdminModule } from './app/admin/admin.module';
 import { DatabaseModule } from './libs/database/database.module';
 
