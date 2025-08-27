fa5308030b812a87d8637627a1627c89638a00d0
Thiago Martins <thiago_valentim1@hotmail.com>
2025-08-27 08:46:39 -0300

    chore: add Nest repl

---
+++ src/repl.ts
@@ -0,0 +1,15 @@
+import { repl } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const replServer = await repl(AppModule);
+  replServer.setupHistory('.nestjs_repl_history', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+bootstrap().catch((error: unknown) => {
+  console.error(error);
+  process.exit(1);
+});
